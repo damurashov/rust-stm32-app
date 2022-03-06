28767dd7c73819a0fc4da21031aa7e86d2a99360
DmitryMurashov <dmtr.murashov@gmail.com>
2022-03-06 21:37:19 +0300

    [:main] Impl | Referencing variables from data sections

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,14 @@
 
 extern crate rust_stm32;
 
+static RODATA_VARIABLE: &[u8] = b"Rodata";
+static mut BSS_VARIABLE: u32 = 0;
+static mut DATA_VARIABLE: u32 = 1;
+
 #[export_name = "main"]
 fn entry() -> ! {
-    loop {}
+	let _rodata = RODATA_VARIABLE;
+	let _bss = unsafe {&BSS_VARIABLE};
+	let _data = unsafe {&DATA_VARIABLE};
+	loop {}
 }
