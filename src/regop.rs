7ffa85325b4fcae422aa4e48a2ae68a9bed54cca
DmitryMurashov <dmtr.murashov@gmail.com>
2022-04-11 14:54:04 +0300

    [src:regop] Impl | Read functions

--- src/regop.rs
+++ src/regop.rs
@@ -1,7 +1,38 @@
-unsafe fn write(val: u32, register: u32) {
-    *(register as *mut u32) = val;
+type TargetUint = u32;
+
+///
+/// \param value    - value to be written into the register
+/// \param register - address of the register to write to
+///
+unsafe fn write(val: TargetUint, register: TargetUint) {
+    *(register as *mut TargetUint) = val;
 }
 
-unsafe fn write_off(val: u32, register: u32, offset: u32) {
-    *(register as *mut u32) = val << offset;
+///
+/// \param value    - value to be written into the register
+/// \param register - address of the register to write to
+/// \param offset   - position of the register
+///
+unsafe fn write_at(val: TargetUint, register: TargetUint, offset: TargetUint) {
+    *(register as *mut TargetUint) = val << offset;
+}
+
+///
+/// \param register - address of the register to read
+///
+/// \return value of the register
+///
+unsafe fn read(register: TargetUint) -> TargetUint {
+    *(register as *const TargetUint)
+}
+
+///
+/// \param register - address of the register to read
+/// \param offset   - offset where the value is stored at
+/// \param mask     - mask to extract the value
+///
+/// \return value of the register
+///
+unsafe fn read_at(register: TargetUint, offset: TargetUint, mask: TargetUint) -> TargetUint {
+    (*(register as *const TargetUint) & mask) >> offset
 }
