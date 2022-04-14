86798a17738d7c884365b0006221bdff817f99b1
DmitryMurashov <dmtr.murashov@gmail.com>
2022-04-14 19:02:44 +0300

    [periph:gpio] Bug | Configuration
    Though working fine, the GPIO corresponding to the LED is set w/ its default values, so its configuration lack required transparency

--- src/periph/gpio.rs
+++ src/periph/gpio.rs
@@ -10,9 +10,9 @@ pub fn configure() {
 	// CubeMX deviates from its naming scheme here, so everything has to be set manually
 	unsafe {
 		regop::write_mask(GPIO_MODER_OUTPUT, reg::GPIOA_BASE + reg::GPIO_MODER_OFFSET,reg::GPIO_MODER_MODER4_MSK);
-		// regop::write_mask(GPIO_OTYPER_OPENDRAIN, reg::GPIOA_BASE + reg::GPIO_OTYPER_OFFSET, reg::GPIO_OTYPER_OT_4);
-		// regop::write_mask(GPIO_OSPEEDR_HIGH, reg::GPIOA_BASE + reg::GPIO_OSPEEDR_OFFSET, reg::GPIO_OSPEEDR_OSPEEDR4_MSK);
-		// regop::write_mask(GPIO_PUPDR_NOPULL, reg::GPIOA_BASE + reg::GPIO_PUPDR_OFFSET, reg::GPIO_PUPDR_PUPDR4_MSK);
-		// regop::write_mask(1, reg::GPIOA_BASE + reg::GPIO_BSRR_OFFSET, reg::GPIO_BSRR_BS_4);
+		regop::write_mask(GPIO_OTYPER_OPENDRAIN, reg::GPIOA_BASE + reg::GPIO_OTYPER_OFFSET, reg::GPIO_OTYPER_OT_4);
+		regop::write_mask(GPIO_OSPEEDR_HIGH, reg::GPIOA_BASE + reg::GPIO_OSPEEDR_OFFSET, reg::GPIO_OSPEEDR_OSPEEDR4_MSK);
+		regop::write_mask(GPIO_PUPDR_NOPULL, reg::GPIOA_BASE + reg::GPIO_PUPDR_OFFSET, reg::GPIO_PUPDR_PUPDR4_MSK);
+		regop::write_mask(0, reg::GPIOA_BASE + reg::GPIO_BSRR_OFFSET, reg::GPIO_BSRR_BS_4);
 	}
 }
