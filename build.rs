26ed5b95175ffb3cdf00d0d8b27d818a6720bba5
DmitryMurashov <dmtr.murashov@gmail.com>
2022-04-23 15:16:43 +0300

    [:build] Cascade

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use std::{error::Error};
 use cc;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    cc::Build::new().file("src/sync.s").compile("asm");
+    cc::Build::new().file("src/thread/sync.s").compile("asm");
     println!("cargo:rerun-if-changed=src/sync.s");
 
     Ok(())
