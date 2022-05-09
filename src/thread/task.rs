1e448fdb0585191ec76981e04e9f9a069d43af90
DmitryMurashov <dmtr.murashov@gmail.com>
2022-05-09 16:34:57 +0300

    [thread:task] Ref | Idiomatic Rust-ian way of checking null-ptrs

--- src/thread/task.rs
+++ src/thread/task.rs
@@ -5,7 +5,7 @@ pub type Runner = &'static dyn Fn() -> ();
 type StackFrame = [u32; 19];
 
 pub enum TaskError {
-	Alloc(usize),  // Could not allocate the memory
+	Alloc,  // Could not allocate the memory
 	MaxNtasks(usize),  // The max. allowed number of tasks has been exceeded
 	NotFound,
 }
@@ -95,26 +95,31 @@ impl Task {
 	fn runner_wrap(id: usize) {
 	}
 
+	fn new() -> Task {
+		return Task {runner: &|| (), stack_begin: 0 as *mut u8, stack_frame: 0 as *mut StackFrame}
+	}
+
+	/// Checks whether memory for the task has been allocated successfully
+	///
+	pub fn is_alloc(&self) -> bool {
+		!(self.stack_begin.is_null() || self.stack_frame.is_null())
+	}
+
 	/// Tries to allocate memory required for the task
 	///
 	pub fn from_stack_size(runner: Runner, stack_size: usize) -> Result<Task, TaskError> {
 		let _critical = sync::Critical::new();
 
 		unsafe {
-			let mut stack_begin = mem::ALLOCATOR.alloc(core::alloc::Layout::from_size_align(stack_size, 4).unwrap());
+			let mut task = Task::new();
+			task.stack_begin = mem::ALLOCATOR.alloc(core::alloc::Layout::from_size_align(stack_size, 4).unwrap());
+			task.stack_frame = mem::ALLOCATOR.alloc(core::alloc::Layout::from_size_align(core::mem::size_of::<StackFrame>(), 4).unwrap()) as *mut StackFrame;
 
-			if stack_begin == 0 as *mut u8 {
-				return Err(TaskError::Alloc(stack_size));
+			if !task.is_alloc() {
+				return Err(TaskError::Alloc)
 			}
 
-			let mut stack_frame = mem::ALLOCATOR.alloc(core::alloc::Layout::from_size_align(core::mem::size_of::<StackFrame>(), 4).unwrap()) as *mut StackFrame;
-
-			if stack_frame == 0 as *mut StackFrame {
-				mem::ALLOCATOR.dealloc(stack_begin, core::alloc::Layout::from_size_align(stack_size, 4).unwrap());
-				return Err(TaskError::Alloc(core::mem::size_of::<StackFrame>()));
-			}
-
-			Ok(Task {runner, stack_begin, stack_frame})
+			Ok(task)
 		}
 	}
 
