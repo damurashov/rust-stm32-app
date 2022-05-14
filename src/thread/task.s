80c591097f008ced21cbff6287e418d83f028524
DmitryMurashov <dmtr.murashov@gmail.com>
2022-05-14 18:37:15 +0300

    [thread:task.s] Impl | Initial asm implementation

---
+++ src/thread/task.s
@@ -0,0 +1,17 @@
+.syntax unified
+
+.global get_stack_frame_next
+.global get_stack_frame_current
+
+_scb_icsr_reg_address: .word 0xE000ED04
+_scb_icsr_pendsvclr: .word (1 << 27)
+
+	.section .text.pend_sv
+	.global pend_sv
+	.type pend_sv, %function
+/* Handles task switching, relying partially on Rust-provided wrappers over task-managing code */
+pend_sv:
+	/* The LD contains EXC_RETURN */
+	push {lr}
+	/* Pop EXC_RETURN, thus endicating end of handler routine */
+	pop {pc}
