53764dfa0ecd53226b476b07097516fc27ec6f90
DmitryMurashov <dmtr.murashov@gmail.com>
2022-05-14 22:06:22 +0300

    [thread:task.s] Impl | Clear interrupt bit

--- src/thread/task.s
+++ src/thread/task.s
@@ -4,7 +4,7 @@
 .global get_stack_frame_current
 
 /* Info for clearing PendSV "interrupt pending" bit */
-_scb_icsr_reg_address: .word 0xE000ED04
+_scb_icsr_address: .word 0xE000ED04
 _scb_icsr_pendsvclr: .word (1 << 27)
 
 	.section .text.pend_sv
@@ -14,5 +14,13 @@ _scb_icsr_pendsvclr: .word (1 << 27)
 pend_sv:
 	/* The LD contains EXC_RETURN */
 	push {lr}
+	/* Disable "interrupt pending" bit (by setting "clear pending") */
+	ldr r1, =_scb_icsr_address
+	ldr r1, [r1]
+	ldr r3, [r1]
+	movs r2, #1
+	lsls r2, #27
+	orrs r3, r2
+	str r3, [r1]
 	/* Pop EXC_RETURN, thus endicating end of handler routine */
 	pop {pc}
